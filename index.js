d57ca8f9b9a3eb8b9cc06d84d66e3e23e5cadb20
yash-deshpande24 <deshpandeyash945@gmail.com>
2025-05-15 18:32:34 +0530

    code push

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+// index.js
 import express from "express";
 import mongoose from "mongoose";
+import { shortUrl, getOriginalUrl } from "./controllers/url.js"; // Make sure this path is correct
+import bodyParser from "body-parser";
+import path from "path";
 
 const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "views"));
 
 mongoose.connect("mongodb://localhost:27017/shorten",
     {
@@ -10,6 +20,15 @@ mongoose.connect("mongodb://localhost:27017/shorten",
 )
     .then(() => { console.log("mongoo to DB"); })
     .catch((err) => { console.log(err); })
+
+//rendering the ejs file
+app.get('/', (req, res) => {
+    res.render('index.ejs', { shortUrl: null });
+})   
+
+app.post("/shorten", shortUrl);
+app.get("/:shortCode", getOriginalUrl);
+
 
 const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
